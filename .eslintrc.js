ed3a78d7c5dcb721bb4e785a35339323c8a81b9e
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-06 23:04:35 -0300

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
