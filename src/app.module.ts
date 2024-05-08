4ed1ac44ecbe236f96690ec096517713812ba288
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-08 02:32:09 -0300

    fix imports errors on app and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 import databaseConfig from './config/database.config';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -16,6 +19,13 @@ import databaseConfig from './config/database.config';
       useFactory: (configService: ConfigService) =>
         configService.get('typeorm'),
     }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
