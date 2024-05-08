0e965c643cbe289d85ace9cee7fd75c24e9704d2
simonsalasseeber@gmail.com <simonsalasseeber@gmail.com>
2024-05-08 15:38:13 +0200

    added payment folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,10 @@ import { UserModule } from './user/user.module';
 import databaseConfig from './config/database.config';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PaymentFlatController } from './payment/payment--flat/payment--flat.controller';
+import { PaymentController } from './payment/payment.controller';
+import { PaymentService } from './payment/payment.service';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -26,8 +30,9 @@ import { JwtModule } from '@nestjs/jwt';
     }),
     UserModule,
     AuthModule,
+    PaymentModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PaymentFlatController, PaymentController],
+  providers: [AppService, PaymentService],
 })
 export class AppModule {}
