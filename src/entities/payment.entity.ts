50d66e92f5a872021e3e5dd68921fe83737ea654
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-09 15:42:46 -0300

    fix on database.config.ts dropSchema: true to dropSchema: false

--- src/entities/payment.entity.ts
+++ src/entities/payment.entity.ts
@@ -1,34 +1,30 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { User } from "./user.entity";
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+// import { User } from './user.entity';
 
 @Entity({
-    name: 'payment',
+  name: 'payment',
 })
-
 export class Payment {
-@PrimaryGeneratedColumn('uuid')
-id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-@Column({
-    type: 'varchar'
-})
-nameOfSubscription: string;
+  @Column({
+    type: 'varchar',
+  })
+  nameOfSubscription: string;
 
-@Column({
-    type: 'varchar'
-})
-price: string;
+  @Column({
+    type: 'varchar',
+  })
+  price: string;
 
-@Column({
-    type: 'varchar'
-})
-currency: string;
+  @Column({
+    type: 'varchar',
+  })
+  currency: string;
 
-// @ManyToOne(() => User, user => user.payment)
-// @JoinColumn({ name: 'user_id' })
+  // @ManyToOne(() => User, user => user.payment)
+  // @JoinColumn({ name: 'user_id' })
 
-// user: User;
-
-
+  // user: User;
 }
