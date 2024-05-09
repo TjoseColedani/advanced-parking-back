7638c8d065a972a4115a8a61cde86128ad203d43
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-09 15:40:49 -0300

    add create appointment workflow

--- src/dtos/Appointments.dto.ts
+++ src/dtos/Appointments.dto.ts
@@ -1,9 +1,9 @@
-export class appointmentDto {
+export class CreateAppointmentDto {
   license_plate: string;
   date: string;
   time: string;
   duration: string;
   is_parked: boolean;
   user_id: string;
-  slot_id: string;
+  parkingLotId: string;
 }
