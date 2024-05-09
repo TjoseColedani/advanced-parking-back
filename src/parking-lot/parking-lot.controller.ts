6348354fa8f0beaf81ab3028fda96cc5d5802d6e
MarioGuTe <mariogutierreztello@gmail.com>
2024-05-09 15:03:21 -0400

    add seeder route

--- src/parking-lot/parking-lot.controller.ts
+++ src/parking-lot/parking-lot.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, OnModuleInit } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  // , OnModuleInit
+} from '@nestjs/common';
 import { ParkingLotService } from './parking-lot.service';
 
 @Controller('parking-lot')
-export class ParkingLotController implements OnModuleInit {
+//  implements OnModuleInit
+export class ParkingLotController {
   constructor(private readonly parkingLotService: ParkingLotService) {}
 
-  async onModuleInit() {
-    await this.parkingLotService.addParkingLots();
+  @Get('seeder')
+  async addParkingLots() {
+    return this.parkingLotService.addParkingLots();
   }
+
+  // async onModuleInit() {
+  //   await this.parkingLotService.addParkingLots();
+  // }
 }
