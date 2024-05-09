09589603ec4f14739a885f876618fd865715f009
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-09 00:34:09 -0300

    add enums folder and slot-status.enum.ts file - update imports

--- src/decorators/roles.decorators.ts
+++ src/decorators/roles.decorators.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
-import { Role } from 'src/roles.enum';
+import { Role } from 'src/enums/roles.enum';
 
 export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
