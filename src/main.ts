5d1e34533aa69664445309faa0b415c2ba96d855
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-16 12:10:21 -0300

    add config on main using ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +10,11 @@ async function bootstrap() {
   });
   app.enableCors();
   app.use(morgan('dev'));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle(`Advanced-Parking App`)
