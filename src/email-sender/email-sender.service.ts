aae6f6ac221a7bd5a40e8c2b83ff5211f6a5acf9
TuriColedani <turihendrix@gmail.com>
2024-05-16 00:10:32 -0300

    add reservationconfirmationemail , reservationreminderemail and add dto to sendregisteremail

--- src/email-sender/email-sender.service.ts
+++ src/email-sender/email-sender.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { EmailSenderRepository } from './email-sender.repository';
+import { EmailSenderDto, RegisterSenderDto } from 'src/dtos/EmailSender.dto';
 
 @Injectable()
 export class EmailSenderService {
   constructor(private readonly emailSenderRepository: EmailSenderRepository) {}
 
-  sendRegisterEmail(name: string, email: string) {
-    return this.emailSenderRepository.sendRegisterEmail(name, email);
+  sendRegisterEmail(registerSenderDto: RegisterSenderDto) {
+    return this.emailSenderRepository.sendRegisterEmail(registerSenderDto);
+  }
+  sendReservationConfirmationEmail(emailSenderDto: EmailSenderDto) {
+    return this.emailSenderRepository.sendReservationConfirmationEmail(
+      emailSenderDto,
+    );
+  }
+
+  sendReservationReminderEmail(emailSenderDto: EmailSenderDto) {
+    return this.emailSenderRepository.sendReservationReminderEmail(
+      emailSenderDto,
+    );
   }
 }
