0a5dd05cc4e7ffc05293a1b46d0cc64d9458c9a6
TuriColedani <turihendrix@gmail.com>
2024-05-16 00:04:04 -0300

    cambiamos el numero de puerto

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    rawBody: true
+    rawBody: true,
   });
   app.enableCors();
   app.use(morgan('dev'));
@@ -18,6 +18,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
