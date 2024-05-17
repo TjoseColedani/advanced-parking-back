ab9f51d8c70e6650f5f200214bef2503644828a3
TuriColedani <turihendrix@gmail.com>
2024-05-17 12:01:35 -0300

    add schedule reminder email related logic and update register message

--- src/email-sender/email-sender.repository.ts
+++ src/email-sender/email-sender.repository.ts
@@ -1,6 +1,7 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { EmailSenderDto, RegisterSenderDto } from 'src/dtos/EmailSender.dto';
+import * as schedule from 'node-schedule';
 
 @Injectable()
 export class EmailSenderRepository {
@@ -11,8 +12,14 @@ export class EmailSenderRepository {
       to: registerSenderDto.email,
       from: 'advancedparking.2024@gmail.com',
       subject: 'Testing Email Sender',
-      text: `Hello ${registerSenderDto.name}, this is a test email.`,
-      html: `<p>Hello ${registerSenderDto.name}, this is a test email in HTML.</p>`,
+      text: `Hello ${registerSenderDto.name}, thank you for registering with us! Your account is now active.
+      Thank you for choosing us!
+      Sincerely,
+      Advanced Parking`,
+      html: `<p>Hello ${registerSenderDto.name}, thank you for registering with us! Your account is now active.</p>
+      <p>Thank you for choosing us!</p>
+      <p>Sincerely,
+      <br>Advanced Parking</p>,`,
     });
 
     return 'Email sent successfully';
@@ -32,7 +39,7 @@ export class EmailSenderRepository {
 
     Thank you for choosing us!
     Sincerely,
-    Parking Name`,
+    Advanced Parking`,
       html: `<p>Hello ${emailSenderDto.name}, your parking reservation has been confirmed. Here are the details:</p>
     <ul>
       <li>Date: ${emailSenderDto.date}</li>
@@ -46,6 +53,27 @@ export class EmailSenderRepository {
     });
   }
 
+  async scheduleReminderEmail(emailSenderDto: EmailSenderDto) {
+    const reservationTime = this.convertToDateTime(
+      emailSenderDto.date,
+      emailSenderDto.time,
+    );
+    console.log(reservationTime, 'reservation time');
+    const reminderTime = new Date(reservationTime.getTime() - 1 * 60000);
+    console.log(reminderTime, 'reminder time');
+
+    schedule.scheduleJob(reminderTime, async () => {
+      await this.sendReservationReminderEmail(emailSenderDto);
+    });
+  }
+
+  private convertToDateTime(date: string, time: string): Date {
+    const [day, month, year] = date
+      .split('/')
+      .map((part) => parseInt(part, 10));
+    const [hours, minutes] = time.split(':').map((part) => parseInt(part, 10));
+    return new Date(year, month - 1, day, hours, minutes);
+  }
   async sendReservationReminderEmail(emailSenderDto: EmailSenderDto) {
     await this.emailSenderRepository.sendMail({
       to: emailSenderDto.email,
@@ -55,13 +83,13 @@ export class EmailSenderRepository {
 
     This is a friendly reminder that you have a reservation in our parking lot for ${emailSenderDto.date} at ${emailSenderDto.time}.
 
-    Don't forget to come!
+    Thank you for choosing us!
 
     Sincerely,
-    Parking Name (booking reminder)`,
+    Advanced Parking`,
       html: `<p>Hello ${emailSenderDto.name}!</p>
     <p>This is a friendly reminder that you have a reservation in our parking lot for ${emailSenderDto.date} at ${emailSenderDto.time}.</p>
-    <p>Don't forget to come!</p>
+    <p>Thank you for choosing us!</p>
     <p>Sincerely,<br>Advanced Parking</p>`,
     });
   }
