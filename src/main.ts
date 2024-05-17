9f6613f6334c1926e33b463e5409ea4dfca511cd
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-16 22:42:56 -0300

    db render ok

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
 }
 bootstrap();
