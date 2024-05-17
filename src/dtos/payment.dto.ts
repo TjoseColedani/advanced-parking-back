64af987c29277417d3e98a9c6ef962cb8999538a
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-05-17 12:33:06 -0300

    add decorators of class validator to paymentDto

--- src/dtos/payment.dto.ts
+++ src/dtos/payment.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 export class CreatePaymentDto {
+  @IsString()
   type_of_service: string;
+  @IsString()
   unit_amount: number;
+  @IsString()
   appointment_id: string;
 }
