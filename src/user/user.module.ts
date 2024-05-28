7f6668831fc292b3a35ff096d700a11eb1ac5a9d
MarioGuTe <mariogutierreztello@gmail.com>
2024-05-28 11:25:36 -0400

    add requestPasswordReset and resetPassword workflow

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,13 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { UserRepository } from './user.repository';
+import { EmailSenderModule } from 'src/email-sender/email-sender.module';
+import { EmailSenderRepository } from 'src/email-sender/email-sender.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), EmailSenderModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
-  exports: [UserRepository]
+  providers: [UserService, UserRepository, EmailSenderRepository],
+  exports: [UserRepository],
 })
 export class UserModule {}
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -4,16 +4,25 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto, UpdateUserDto } from 'src/dtos/user.dto';
+import {
+  CreateUserDto,
+  ResetPasswordDto,
+  UpdateUserDto,
+  UserEmailDto,
+} from 'src/dtos/user.dto';
 import { User } from 'src/entities/user.entity';
 import { Role } from 'src/enums/roles.enum';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { EmailSenderRepository } from 'src/email-sender/email-sender.repository';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserRepository {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+    private readonly emailSenderRepository: EmailSenderRepository,
   ) {}
   async getAllUsers(): Promise<User[]> {
     const users = await this.userRepository.find();
@@ -102,5 +111,49 @@ export class UserRepository {
     });
 
     return createdUser;
+  }
+
+  async requestPasswordReset(userEmailDto: UserEmailDto): Promise<void> {
+    const user = await this.userRepository.findOne({
+      where: { email: userEmailDto.email },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const token = this.jwtService.sign(
+      { userId: user.id },
+      { expiresIn: '1h' },
+    );
+
+    const resetLink = `http://example.com/reset/${token}`;
+
+    await this.emailSenderRepository.sendPasswordResetEmail(
+      resetLink,
+      user.email,
+    );
+  }
+
+  async resetPassword(resetPasswordDto: ResetPasswordDto): Promise<void> {
+    const { token, newPassword } = resetPasswordDto;
+    let payload;
+    console.log(payload);
+    try {
+      payload = this.jwtService.verify(token);
+    } catch (error) {
+      throw new BadRequestException('Invalid or expired token');
+    }
+
+    const user = await this.userRepository.findOne({
+      where: { id: payload.userId },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await this.userRepository.save(user);
   }
 }
