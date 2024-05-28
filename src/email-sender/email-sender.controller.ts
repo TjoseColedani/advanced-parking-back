bc9498e09fbcb238060d781d9688dc079f3bd424
simonsalasseeber@gmail.com <simonsalasseeber@gmail.com>
2024-05-28 11:51:43 +0200

    added swagger documentation to reviews and email

--- src/email-sender/email-sender.controller.ts
+++ src/email-sender/email-sender.controller.ts
@@ -5,16 +5,20 @@ import {
   RegisterSenderDto,
   UserFormSenderDto,
 } from 'src/dtos/EmailSender.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('emails')
 @Controller('email-sender')
 export class EmailSenderController {
   constructor(private readonly emailSenderService: EmailSenderService) {}
   @Post('registered')
+  @ApiOperation({summary: 'Send register email'})
   sendRegisterEmail(@Body() registerSenderDto: RegisterSenderDto) {
     return this.emailSenderService.sendRegisterEmail(registerSenderDto);
   }
 
   @Post('confirmed')
+  @ApiOperation({summary: 'Send reservation confirmed email'})
   sendRervationConfirmedEmail(
     @Body()
     emailSenderDto: EmailSenderDto,
@@ -25,6 +29,7 @@ export class EmailSenderController {
   }
 
   @Post('reminder-email')
+  @ApiOperation({summary: 'Send reservation reminder email'})
   sendReservationReminderEmail(
     @Body()
     emailSenderDto: EmailSenderDto,
@@ -33,11 +38,13 @@ export class EmailSenderController {
   }
 
   @Post('schedule-reminder')
+  @ApiOperation({summary: 'Send schedule reminder email'})
   scheduleReminderEmail(@Body() emailSenderDto: EmailSenderDto) {
     return this.emailSenderService.scheduleReminderEmail(emailSenderDto);
   }
 
   @Post('contact-form')
+  @ApiOperation({summary: 'Send  user contact form email'})
   async sendUserContactForm(@Body() userFormSenderDto: UserFormSenderDto) {
     await this.emailSenderService.sendUserContactForm(userFormSenderDto);
     return 'Test notification email sent successfully';
--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -10,27 +10,33 @@ import {
 } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto, UpdateReviewDto } from 'src/dtos/Review.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('reviews')
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
   @Post()
+  @ApiOperation({summary: 'Create a review'})
   create(@Body() createReviewDto: CreateReviewDto) {
     return this.reviewsService.create(createReviewDto);
   }
 
   @Get()
+  @ApiOperation({summary: 'Get all reviews'})
   findAll() {
     return this.reviewsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({summary: 'Get review by id'})
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.reviewsService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({summary: 'Update review by id'})
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateReviewDto: UpdateReviewDto,
@@ -39,6 +45,7 @@ export class ReviewsController {
   }
 
   @Delete(':id')
+  @ApiOperation({summary: 'Delete review by id'})
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.reviewsService.remove(id);
   }
